6a011c1babf4dd851c8495b0adf7af1ad69cb926
Ahmed samy <Ahmed5-5>
2023-03-21 16:49:20 +0200

    add jop fields and Category

--- jop/models.py
+++ jop/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+class jop(models.Model):
+    '''
+    Django_models_feilds:
+        -Html_wedjet
+        -validation
+        -db size
+    '''
+    JOP_TITLE = (
+        ("Full_time","Full_time"),("Part_time","Part_time")
+    )
+
+    title = models.CharField(max_length=50)
+   # location = models.CharField(max_length=20)
+    jop_title = models.CharField(max_length=20,choices=JOP_TITLE)
+    description = models.TextField(max_length=500)
+    published_at = models.DateTimeField(auto_now=True)
+    vacancy = models.IntegerField(default=1)
+    salary = models.IntegerField(default=0)
+    experience = models.IntegerField(default=1)
+    category = models.ForeignKey('Category' , on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+    
+class Category(models.Model):
+    name = models.CharField(max_length= 20)
+
+    def __str__(self):
+        return self.name
